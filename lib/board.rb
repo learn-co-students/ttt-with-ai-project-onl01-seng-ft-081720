46b3b97aff37545aa156130953822cb10d0f50da
Newland-A <ajn252@gmail.com>
2020-09-06 19:11:38 -0400

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,52 @@
+class Board
+
+  attr_accessor :cells
+
+  @cells = []
+
+  def reset!
+    self.cells = Array.new(9, " ")
+  end
+
+  def initialize
+    self.cells = Array.new(9, " ")
+  end
+
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+  
+  def position(user_input)
+    self.cells[user_input.to_i-1]
+  end
+
+  def full?
+    #if the board is full 
+    self.cells.all? {|cell| cell == "X" || cell == "O"}
+  end
+
+  def turn_count
+    #counts the amount of x's and the amount of O's on the board
+    self.cells.count("X") + self.cells.count("O")
+  end
+
+  def taken?(position) #check board position
+    self.cells[position.to_i-1] == "X" || #board position -1 the computer starts counting at zero
+    self.cells[position.to_i-1] == "O"
+  end
+
+  def valid_move?(position)
+    #makes sure that the positon is greater than zero and less than 9
+    !taken?(position) && position.to_i > 0 && position.to_i <= 9
+  end
+
+  def update(position, player)
+    #updates the board with the apporpriate marker and set it equal to the players token
+      self.cells[position.to_i-1] = player.token
+  end
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,7 @@
+module Players
+  class Human < Player  
+    def move(board)
+      input = gets.chomp #or strip they both work
+    end
+  end
+end
